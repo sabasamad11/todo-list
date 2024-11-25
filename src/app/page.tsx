a62d565fe4d1173835c537c18dcf63c85918182e
sabasamad11 <s10362084@gmail.com>
2024-11-25 14:48:15 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoList from "@/components/todo-list";
+
+const page = () => {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  )
+}
+
+export default page
